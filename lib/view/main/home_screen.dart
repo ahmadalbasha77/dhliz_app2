27ba4e73d643e1f247eaf8589911dcc60cd44e05
ahmad <ahmadalbasha2011@gmail.com>
2024-04-15 13:20:59 +0300

    edit size home screen

--- lib/view/main/home_screen.dart
+++ lib/view/main/home_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-
 import '../../config/utils.dart';
 import '../home/account_monitoring_screen.dart';
 import '../home/self mangement of invntory/self_management_of_inventory_screen.dart';
-import '../../config/shared_prefs_client.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -18,6 +17,11 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
+    final isTablet = screenSize.shortestSide >= 600; // Tablet threshold
+
+    double basePadding = isTablet ? 20 : 11;
+    double titleFontSize = isTablet ? 24 : 16;
+    double descriptionFontSize = isTablet ? 16 : 12;
 
     return WillPopScope(
       onWillPop: () async {
@@ -32,147 +36,60 @@ class HomeScreen extends StatelessWidget {
           backgroundColor: Colors.white,
           title: Text(
             "Home".tr,
-            style: TextStyle(color: Colors.black),
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: screenSize.width * (isTablet ? 0.03 : 0.045)),
           ),
           centerTitle: true,
         ),
         body: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                margin: EdgeInsets.symmetric(
-                    horizontal: screenSize.width * 0.05,
-                    vertical: screenSize.height * 0.01),
+              Padding(
+                padding: EdgeInsets.all(basePadding),
                 child: Text(
                   "Inventory".tr,
                   style: TextStyle(
-                    fontSize: screenSize.width * 0.05,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w500,
-                  ),
+                      fontSize: screenSize.width * (isTablet ? 0.042 : 0.045),
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500),
                 ),
               ),
-              // SizedBox(
-              //   height: screenSize.height * 0.002,
-              // ),
-              _buildInventoryItem(
+              _buildInventoryCard(
+                  context,
                   screenSize,
+                  "SELF.png",
                   "Self management of inventory".tr,
                   "you can enter, withdraw, and transfer the inventory alone"
                       .tr,
-                  'image/home/SELF.png',
-                  () => Get.to(() => const SelfManagementOfInventoryScreen()),
-                  'control panel',
-                  Icon(
-                    Icons.apps,
-                    color: Colors.black,
-                    size: screenSize.width * 0.045,
-                  )),
-              SizedBox(
-                height: screenSize.height * 0.01,
-              ),
-              _buildInventoryItem(
-                screenSize,
-                "call customer care".tr,
-                "you can add your inventory by customer care".tr,
-                'image/home/Customer careCustomer care.png',
-                _callNumber,
-                'call now',
-                Icon(
+                  Icons.apps_rounded,
+                  "control panel".tr),
+              _buildInventoryCard(
+                  context,
+                  screenSize,
+                  "Customer careCustomer care.png",
+                  "call customer care".tr,
+                  "you can add your inventory by customer care".tr,
                   Icons.call,
-                  color: Colors.black,
-                  size: screenSize.width * 0.045,
-                ),
-              ),
-              SizedBox(
-                height: screenSize.height * 0.02,
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(
-                    horizontal: screenSize.width * 0.05,
-                    vertical: screenSize.width * 0.02),
+                  "call now".tr),
+              Padding(
+                padding: EdgeInsets.all(basePadding),
                 child: Text(
                   "Monitoring".tr,
                   style: TextStyle(
-                    fontSize: screenSize.width * 0.04,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w500,
-                  ),
+                      fontSize: screenSize.width * (isTablet ? 0.042 : 0.045),
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500),
                 ),
               ),
-              InkWell(
-                onTap: () {
-                  print('************************************');
-                  print(sharedPrefsClient.accessToken);
-
-                  print('************************************');
-                  Get.to(() => const StockMonitoringScreen());
-                },
-                child: Container(
-                  margin:
-                      EdgeInsets.symmetric(horizontal: screenSize.width * 0.04),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Color.fromRGBO(80, 46, 144, 1.0),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            margin: EdgeInsets.all(screenSize.width * 0.045),
-                            child: Text(
-                              "Account Monitoring".tr,
-                              style: TextStyle(
-                                fontSize: screenSize.width * 0.048,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                          Container(
-                            width: 170,
-                            margin: EdgeInsets.symmetric(
-                                horizontal: screenSize.width * 0.035),
-                            child: Text(
-                              "Analysis of your inventory can be found".tr,
-                              style: TextStyle(
-                                fontSize: screenSize.width * 0.03,
-                                color: Colors.white30,
-                              ),
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.symmetric(
-                              vertical: screenSize.width * 0.045,
-                              horizontal: screenSize.width * 0.06,
-                            ),
-                            child: Text(
-                              "view".tr,
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: screenSize.width * 0.035),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        margin: EdgeInsets.all(screenSize.width * 0.005),
-                        child: Image.asset(
-                          'image/home/analytics.png',
-                          width: screenSize.width * 0.33,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: screenSize.width * 0.045,
-              )
+              _buildMonitoringCard(
+                  context,
+                  screenSize,
+                  "analytics.png",
+                  "Account Monitoring".tr,
+                  "Analysis of your inventory can be found".tr,
+                  "view".tr),
             ],
           ),
         ),
@@ -180,31 +97,28 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildInventoryItem(
-    Size screenSize,
-    String title,
-    String description,
-    String imagePath,
-    Function() onTap,
-    String text,
-    Icon icon,
-  ) {
+  Widget _buildInventoryCard(
+      BuildContext context,
+      Size screenSize,
+      String imagePath,
+      String title,
+      String subtitle,
+      IconData icon,
+      String iconText) {
+    bool isTablet = screenSize.width > 600;
+    double widthMultiplier = isTablet ? 0.04 : 0.05;
+
     return Container(
-      height: screenSize.height * 0.18,
-      padding: EdgeInsets.symmetric(
-        vertical: screenSize.width * 0.025,
-        horizontal: screenSize.width * 0.01,
-      ),
+      height: screenSize.width * (isTablet ? 0.3 : 0.3),
+      margin: EdgeInsets.symmetric(
+          horizontal: screenSize.width * widthMultiplier, vertical: 10),
+      padding: EdgeInsets.all(10.w),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(15),
       ),
-      margin: EdgeInsets.symmetric(
-        vertical: screenSize.width * 0.01,
-        horizontal: screenSize.width * 0.04,
-      ),
       child: InkWell(
-        onTap: onTap,
+        onTap: () => Get.to(() => const SelfManagementOfInventoryScreen()),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -212,59 +126,90 @@ class HomeScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Container(
-                  child: Text(
-                    title,
+                Text(title.tr,
                     style: TextStyle(
-                      fontSize: screenSize.width * 0.044,
-                    ),
-                  ),
-                  margin: EdgeInsets.symmetric(
-                    horizontal: screenSize.width * 0.03,
-                    vertical: screenSize.width * 0.02,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: screenSize.width * 0.035,
-                  ),
-                  child: Container(
-                    width: screenSize.width * 0.48,
-                    child: Text(
-                      description,
+                        fontSize:
+                            screenSize.width * (isTablet ? 0.04 : 0.042))),
+                SizedBox(
+                  width: screenSize.width * (isTablet ? 0.5 : 0.5),
+                  child: Text(subtitle.tr,
                       style: TextStyle(
-                        fontSize: screenSize.width * 0.029,
-                        color: Colors.black38,
-                      ),
-                    ),
-                  ),
+                          fontSize:
+                              screenSize.width * (isTablet ? 0.022 : 0.028),
+                          color: Colors.black38)),
+                ),
+                Row(
+                  children: [
+                    Icon(icon,
+                        color: Colors.black,
+                        size: screenSize.width * (isTablet ? 0.04 : 0.045)),
+                    SizedBox(width: 5),
+                    Text(iconText.tr,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize:
+                                screenSize.width * (isTablet ? 0.027 : 0.027))),
+                  ],
+                )
+              ],
+            ),
+            Image.asset('image/home/$imagePath', width: screenSize.width * 0.25)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMonitoringCard(BuildContext context, Size screenSize,
+      String imagePath, String title, String subtitle, String buttonText) {
+    bool isTablet = screenSize.width > 600;
+    double widthMultiplier = isTablet ? 0.075 : 0.05;
+
+    return InkWell(
+      onTap: () => Get.to(() => const StockMonitoringScreen()),
+      child: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: screenSize.width * (isTablet ? 0.04 : 0.05),
+        ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Color.fromRGBO(80, 46, 144, 1.0)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(screenSize.width * 0.045),
+                  child: Text(title.tr,
+                      style: TextStyle(
+                          fontSize: screenSize.width * 0.048,
+                          color: Colors.white)),
                 ),
                 Container(
-                  margin: EdgeInsets.only(
-                    top: screenSize.height * 0.0082,
-                    left: screenSize.width * 0.045,
-                    right: screenSize.width * 0.045,
-                  ),
-                  child: Row(
-                    children: [
-                      icon,
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        text.tr,
-                        style: TextStyle(color: Colors.black),
-                      )
-                    ],
-                  ),
+                  margin: EdgeInsets.symmetric(
+                      horizontal: screenSize.width * (isTablet ? 0.035 : 0.04)),
+                  width: screenSize.width * (isTablet ? 0.4 : 0.4),
+                  child: Text(subtitle.tr,
+                      style: TextStyle(
+                          fontSize:
+                              screenSize.width * (isTablet ? 0.028 : 0.032),
+                          color: Colors.white30)),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: screenSize.width * 0.045,
+                      horizontal: screenSize.width * 0.045),
+                  child: Text(buttonText.tr,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize:
+                              screenSize.width * (isTablet ? 0.035 : 0.03))),
                 ),
               ],
             ),
-            Image.asset(
-              imagePath,
-              width: screenSize.width * 0.22,
-              height: screenSize.height * 0.13,
-            )
+            Image.asset('image/home/$imagePath', width: screenSize.width * 0.3),
           ],
         ),
       ),
